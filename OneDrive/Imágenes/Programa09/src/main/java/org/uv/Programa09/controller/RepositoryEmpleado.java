ae72f01d5a2e14d43c5fff9cc713f0c5ef88b157
AldoTR <aldotores10@gmail.com>
2024-01-29 17:09:47 -0600

    Delete OneDrive directory

--- OneDrive/Imágenes/Programa09/src/main/java/org/uv/Programa09/controller/RepositoryEmpleado.java
+++
@@ -1,9 +0,0 @@
-package org.uv.Programa09.controller;
-
-import org.springframework.data.repository.CrudRepository;
-
-
-public interface RepositoryEmpleado extends CrudRepository<Empleado, Long>{
-    
-}
-
